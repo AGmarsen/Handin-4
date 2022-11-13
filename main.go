ba1640c2969aeafd10f1f132a4fa7fea66cc48b8
tcla <tcla@itu.dk>
2022-11-13 12:51:15 +0100

    Seems working. Untested

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"context"
 	"fmt"
-	peer "github.com/AGmarsen/Handin-4/proto"
+	voter "github.com/AGmarsen/Handin-4/proto"
 	"google.golang.org/grpc"
 	"log"
 	"net"
@@ -29,7 +29,7 @@ func main() {
 		id:      ownPort,
 		clock:   0,
 		state:   RELEASED,
-		clients: make(map[int32]peer.VoteClient),
+		clients: make(map[int32]voter.VoteClient),
 		ctx:     ctx,
 	}
 
@@ -38,8 +38,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to listen on port: %v", err)
 	}
-	peerServer := peer.NewServer()
-	peer.RegisterVoteServer(peerServer, p)
+	peerServer := grpc.NewServer()
+	voter.RegisterVoteServer(peerServer, p)
 
 	go func() {
 		if err := peerServer.Serve(list); err != nil {
@@ -54,77 +54,76 @@ func main() {
 			continue
 		}
 
-		var conn *peer.ClientConn
-		fmt.Printf("Trying to dial: %v\n", port)
-		conn, err := peer.Dial(fmt.Sprintf(":%v", port), peer.WithInsecure(), peer.WithBlock())
+		var conn *grpc.ClientConn
+		log.Printf("Trying to dial: %v\n", port)
+		conn, err := grpc.Dial(fmt.Sprintf(":%v", port), grpc.WithInsecure(), grpc.WithBlock())
 		if err != nil {
 			log.Fatalf("Could not connect: %s", err)
 		}
 		defer conn.Close()
-		c := peer.NewVoteClient(conn)
+		c := voter.NewVoteClient(conn)
 		p.clients[port] = c
 	}
 
 	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
+	for scanner.Scan() { //press enter to request critical state
 		p.enter()
 	}
 }
 
 type peer struct {
-	peer.UnimplementedVoteServer
+	voter.UnimplementedVoteServer
 	id      int32
 	clock   int32
 	state   int
-	clients map[int32]peer.VoteClient
+	clients map[int32]voter.VoteClient
 	ctx     context.Context
 	mutex   sync.Mutex
 }
 
-func (p *peer) Request(ctx context.Context, req *peer.Request) (*peer.Response, error) {
-	updateLamport(req)
-	log.Printf("Request received from %d (%d, %d)\n", req.id, p.id, p.clock)
-	var resp peer.Response
-	for p.state == HELD {
-		//don't respond while in critical state
+func (p *peer) Vote(ctx context.Context, req *voter.Request) (*voter.Response, error) {
+	p.updateLamportReq(req)
+	log.Printf("Request received from %d L(%d, %d)\n", req.Id, p.id, p.clock)
+	for {
+		if p.state != HELD { //don't respond while in critical state
+			break
+		}
 	}
-	if p.state == RELEASED {
-		resp = &peer.Response{id: p.id, p.clock}
-		updateLamport(resp)
-		resp = &peer.Response{id: p.id, p.clock}
-		return resp, nil
-	}
-	//if wanted and request has higher priority
-	if p.clock > req.clock || p.clock == req.clock && p.id > req.id {
-		resp = &peer.Response{id: p.id, p.clock}
-		updateLamport(resp)
-		resp = &peer.Response{id: p.id, p.clock}
+	//if i dont want it or we both want it but they have a better Lamport (better = lower)
+	if p.state == RELEASED || (p.clock > req.Clock || p.clock == req.Clock && p.id > req.Id){
+		resp := &voter.Response{Id: p.id, Clock: p.clock}
+		p.updateLamportResp(resp)
+		resp = &voter.Response{Id: p.id, Clock: p.clock}
+		log.Printf("Respond to %d L(%d, %d)\n", req.Id, resp.Id, resp.Clock)
 		return resp, nil
 	}
 	//else if I have higher priority
-	for state != RELEASED {
-		//don't respond until I'm are done
+	for  {
+		if p.state == RELEASED { //don't respond until I'm done
+			break
+		}
 	}
-	resp = &peer.Response{id: p.id, p.clock}
-	updateLamport(resp)
-	resp = &peer.Response{id: p.id, p.clock}
+	resp := &voter.Response{Id: p.id, Clock: p.clock}
+	p.updateLamportResp(resp)
+	resp = &voter.Response{Id: p.id, Clock: p.clock}
+	log.Printf("Respond to %d L(%d, %d)\n", req.Id, resp.Id, resp.Clock)
 	return resp, nil
 }
 
 // try to enter critical state
 func (p *peer) enter() {
 	p.state = WANTED
-	request := &peer.Request{id, clock}
+	request := &voter.Request{Id: p.id, Clock: p.clock}
 
 	for id, client := range p.clients {
-		updateLamport(request)
-		log.Printf("Request sent to: %d (%d, %d)\n", id, p.id, p.clock)
-		reply, err := client.Request(request)
+		p.updateLamportReq(request)
+		log.Printf("Request sent to: %d L(%d, %d)\n", id, p.id, p.clock)
+		reply, err := client.Vote(p.ctx, request)
 		if err != nil {
-			log.Printf(err)
+			log.Printf("%v", err)
 		}
-		updateLamport(reply)
-		log.Printf("Reply recieved from %d (%d, %d)\n", reply.id, p.id, p.clock)
+		p.updateLamportResp(reply)
+		log.Printf("Reply recieved from %d L(%d, %d)\n", reply.Id, p.id, p.clock)
 	}
 	//after response from two peers
 	p.state = HELD
@@ -134,26 +133,25 @@ func (p *peer) enter() {
 
 	//Release Critical
 	p.state = RELEASED
-
 }
 
 func doCriticalStuff() {
-	log.Printf("I got permission :D")
+	log.Printf("Critical: I got permission :D")
 }
 
-func (p *Peer) updateLamport(req *peer.Request) {
-	p.mutex.lock()
-	defer p.mutex.unlock()
-	req.clock = max(p.clock, req.clock) + 1
+func (p *peer) updateLamportReq(req *voter.Request) {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+	p.clock = max(p.clock, req.Clock) + 1
 }
 
-func (p *Peer) updateLamport(resp *peer.Response) {
-	p.mutex.lock()
-	defer p.mutex.unlock()
-	req.clock = max(p.clock, resp.clock) + 1
+func (p *peer) updateLamportResp(resp *voter.Response) {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+	p.clock = max(p.clock, resp.Clock) + 1
 }
 
-func max(a int32, b int32) {
+func max(a int32, b int32) int32 {
 	if a > b {
 		return a
 	}
